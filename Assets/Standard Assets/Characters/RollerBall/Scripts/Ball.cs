d820fc63ba2575fec9d1c943a4c4bab5eddcefbb
DESKJULES\Admin <lordstok@hotmail.com>
2016-11-09 21:12:53 +1100

    lecture 272

--- Assets/Standard Assets/Characters/RollerBall/Scripts/Ball.cs
+++ Assets/Standard Assets/Characters/RollerBall/Scripts/Ball.cs
@@ -8,7 +8,7 @@ namespace UnityStandardAssets.Vehicles.Ball
         [SerializeField] private float m_MovePower = 5; // The force added to the ball to move it.
         [SerializeField] private bool m_UseTorque = true; // Whether or not to use torque to move the ball.
         [SerializeField] private float m_MaxAngularVelocity = 25; // The maximum velocity the ball can rotate at.
-        [SerializeField] private float m_JumpPower = 2; // The force added to the ball when it jumps.
+        [SerializeField] private float m_JumpPower = 1; // The force added to the ball when it jumps.
 
         private const float k_GroundRayLength = 1f; // The length of the ray to check if the ball is grounded.
         private Rigidbody m_Rigidbody;
