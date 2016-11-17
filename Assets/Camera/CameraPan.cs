33158c8406e6b43c51f7ac3aeadba6370cbf0e3c
DESKJULES\Admin <lordstok@hotmail.com>
2016-11-17 21:52:01 +1100

    End lectures - final

---
+++ Assets/Camera/CameraPan.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPan : MonoBehaviour {
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		if (player) {
+			print ("found player");
+		} else {
+			print ("could not find player");
+		}
+	}
+
+	void LateUpdate () {
+		transform.LookAt (player.transform);
+	}
+}
---
+++ Assets/GAME/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GameManager : MonoBehaviour {
+
+	public bool recording = true;
+	private float initialFixedDeltaTime;
+	private bool isPaused;
+
+	// Use this for initialization
+	void Start () {
+		initialFixedDeltaTime = Time.fixedDeltaTime;
+		print ("Initial fixedDeltaTime = " + initialFixedDeltaTime);
+		isPaused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (CrossPlatformInputManager.GetButton("Fire1")){
+			recording = false;
+		} else {
+			recording = true;
+		}
+
+		if (Input.GetKeyDown (KeyCode.P)) {
+			PauseGame ();
+		}
+
+		if (Input.GetKeyDown (KeyCode.R)) {
+			ResumeGame ();
+		}
+
+	}
+
+	void ResumeGame () {
+		isPaused = false;
+		Time.timeScale = 1f;
+		Time.fixedDeltaTime = initialFixedDeltaTime;
+	}
+
+	void PauseGame () {
+		isPaused = true;
+		Time.timeScale = 0;
+		Time.fixedDeltaTime = 0;
+	}
+
+	void OnApplicationPause (bool pauseStatus) {
+		isPaused = pauseStatus;
+	}
+}
